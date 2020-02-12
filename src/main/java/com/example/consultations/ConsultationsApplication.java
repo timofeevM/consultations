651df55c802933cfecbfafe9a1a440571144dd27
TimofeevM <you@example.com>
2020-02-12 10:55:01 +0500

    #1: Project structure

---
+++ src/main/java/com/example/consultations/ConsultationsApplication.java
@@ -0,0 +1,13 @@
+package com.example.consultations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultationsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsultationsApplication.class, args);
+    }
+
+}
